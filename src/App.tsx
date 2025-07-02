ad85cbc1f97ae46501f95f5c286e89ef49ebdbae
yu64 <yumaikuto1212@gmail.com>
2025-07-03 07:52:10 +0900

    動画を流すサンプルを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { useRef, useState } from "react";
+
 function App()
 {
+  const [file, setFile] = useState<File | null>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
+  
+  if(file && videoRef.current)
+  {
+    const url = URL.createObjectURL(file);
+    videoRef.current.src = url;
+    videoRef.current.play();
+  }
+
   return <>
     <h1>Working!</h1>
+    <input type="file" accept="video/*" onChange={(e) => setFile(e.target.files?.[0] ?? null)}></input>
+    <video controls width={480} ref={videoRef}></video>
   </>;
 }
 
